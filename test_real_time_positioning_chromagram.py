c13b451fcf10ec6cce75ae7779ded0ff92cea442
9552nZ <alexis.nortier@edhec.com>
2017-05-06 12:44:19 +0100

    Add files via upload

--- test_real_time_positioning_chromagram.py
+++ test_real_time_positioning_chromagram.py
@@ -13,8 +13,9 @@ class Position():
     
     min_data_sec = 3 # min volume of data needed before processing (in secs)
     data_buffer_sec = 4 # volume of data we keep to do the comparison
+    proba_redistribution_fract = 0.2 # the fraction of probability mass that goes to unlikely positions
         
-    def __init__(self, wd, filename, samplerate, win_s, hop_s, verbose =  False):   
+    def __init__(self, wd, filename, samplerate, win_s, hop_s, verbose =  False, plot_online = False):   
         
         # Fixed parameters
         self.sample_rate_est = samplerate         
@@ -31,20 +32,27 @@ class Position():
         self.sample_rate_act = spec_data_act["samplerate"]
         self.win_s_act = spec_data_act["win_s"]
         self.hop_s_act = spec_data_act["hop_s"]
+        self.len_chromagram_sec = self.chromagram_act.shape[0] * self.hop_s_act / float(self.sample_rate_act) 
         
         # Initialise audio_data to an empty list
         self.audio_data = [] 
         
         # Initialise the total recording time to 0
-        self.total_recording_secs = 0.0
+        self.total_recording_sec = 0.0
         
         # Initialise positions probabilities with equi-probabilities
         probas = np.ones(spec_data_act["spectrogram"].shape[0]) / float(spec_data_act["spectrogram"].shape[0])
-        time_secs = np.arange(spec_data_act["spectrogram"].shape[0]) * spec_data_act["hop_s"] / float(spec_data_act["samplerate"]) 
-        self.position_proba = utils.relative_ts_to_absolute_ts(time_secs, probas)  
+        time_sec = np.arange(spec_data_act["spectrogram"].shape[0]) * spec_data_act["hop_s"] / float(spec_data_act["samplerate"]) 
+        self.position_proba = utils.relative_ts_to_absolute_ts(time_sec, probas)  
         
         # Calibrate the noise level
         self.calibrate_noise_level()
+        
+        # Axis placeholder for plotting
+        if plot_online:
+            plt.ion()
+            _, gca = plt.subplots()
+            self.gca = gca
         
     def calibrate_noise_level(self):
         
@@ -70,7 +78,14 @@ class Position():
         
         self.noise_level = np.percentile(dists, 5.0)
         
-        self.profiling_report += "%s seconds for noise calibration\r" % (time.time()-start_time)                              
+        self.profiling_report += "%0.4fs seconds for noise calibration\r" % (time.time()-start_time)
+    
+    def plot_online(self):
+        
+        utils.plot_chromagram(self.chromagram_est, self.sample_rate_est, self.hop_s_est, self.gca)
+        plt.pause(0.05)
+        
+        
         
     def update_audio_data(self, new_audio_data):
         
@@ -98,7 +113,7 @@ class Position():
         self.timedelta = len_new_audio_data / float(self.sample_rate_est)
         
         # Keep track of the total recording time
-        self.total_recording_secs += self.timedelta             
+        self.total_recording_sec += self.timedelta             
      
     def distance_to_proba(self, ts_dist, conversion_type = 'inverse_denoised'):
         
@@ -116,6 +131,14 @@ class Position():
         # Normalise to get a well-defined probability measure
         ts_proba = ts_proba / np.sum(ts_proba)
         
+#         ordered_position_proba = ts_proba.order(ascending = False)
+#         print ordered_position_proba[0:5] 
+        
+        # Restribute a bit of the probabilities mass towards unlikely positions 
+        # (this should break if conversion_type != 'inverse_denoised')        
+        ts_proba[ts_proba.index.difference(idxs_noise)] = (1.0 - Position.proba_redistribution_fract) * ts_proba[ts_proba.index.difference(idxs_noise)].values
+        ts_proba[idxs_noise] = Position.proba_redistribution_fract * np.ones(len(idxs_noise)) / float(len(idxs_noise))   
+        
         self.position_proba_new = ts_proba
         
     def update_position_proba(self):
@@ -124,25 +147,28 @@ class Position():
         # has has been received since last update 
         position_proba_old = self.position_proba.shift(1, freq = dt.timedelta(seconds = self.timedelta))
         
-        # Take the intersection of the indices (may need to be removed later)        
-        idxs = self.position_proba_new.index.intersection(position_proba_old.index)
+        # Build the search range and filter the two proba times series        
+        min_search = dt.timedelta(seconds=self.total_recording_sec) + utils.START_DATETIME 
+        max_search = dt.timedelta(seconds=self.len_chromagram_sec-self.len_audio_data_sec) + utils.START_DATETIME 
+        position_proba_old = position_proba_old[np.logical_and(position_proba_old.index >= min_search, position_proba_old.index <= max_search)]
+        position_proba_new = self.position_proba_new[np.logical_and(self.position_proba_new.index >= min_search, self.position_proba_new.index <= max_search)]
+        
+        # Fit the new probas to the old probas 
+        # (the timedelta in the timestamps is non-zero only because of numerical errors)   
+        position_proba_new = position_proba_new.asof(position_proba_old.index)
         
         # Multiply the prior probability and current measurement 
         # to get the new probability
-        position_proba_updated = np.multiply(position_proba_old[idxs].values, self.position_proba_new[idxs].values)
+        position_proba_updated = position_proba_old.multiply(position_proba_new)
         
         # Renormalise
-        position_proba_updated = position_proba_updated / np.sum(position_proba_updated) 
-        
-        self.position_proba = pd.Series(position_proba_updated, idxs)                        
+        self.position_proba = position_proba_updated / np.sum(position_proba_updated) 
+                                
             
     def find_position(self, new_audio_data): 
         
         # Start by adding the new audio data to the existing data        
         Position.update_audio_data(self, new_audio_data)
-        
-        # Update the search range
-        Position.update_search_range(self) 
         
         # Check if we have enough data to run the position algorithm
         if self.len_audio_data_sec > Position.min_data_sec:
@@ -170,15 +196,17 @@ class Position():
             Position.update_position_proba(self)
             proba_time = time.time() - start_time
             
-            # Print report if need be !!! INCLUDE IN PROFILING REPORT!!
-            if self.verbose:
-                print("%s seconds for spectrogram" % spectrogram_time)
-                print("%s seconds for chromagram" % chromagram_time)
-                print("%s seconds for comparison" % comparison_time)
-                print("%s seconds for proba update" % proba_time)
+            # Add the timing informations to the profiling report
+            self.profiling_report += "%0.4fs seconds for spectrogram\r" % spectrogram_time
+            self.profiling_report += "%0.4fs seconds for chromagram\r" % chromagram_time 
+            self.profiling_report += "%0.4fs seconds for comparison\r" % comparison_time 
+            self.profiling_report += "%0.4fs seconds for proba update\r" % proba_time  
                 
             ordered_position_proba = self.position_proba.order(ascending = False)
             print ordered_position_proba[0:5] #ts_dist.idxmin()
+            
+#             Position.plot_chromagrame_est_online(self)
+
 
 def callback(in_data, frame_count, time_info, flag):
     
@@ -188,8 +216,6 @@ def callback(in_data, frame_count, time_info, flag):
     pos.find_position(audio_data)    
     
     return(('', False))
-
-
 
 wd = "C:/Users/Alexis/Desktop/Business/SmartSheetMusic/Samples/Nocturnes/"
 filename = "nocturnes" 
@@ -205,32 +231,37 @@ HOP_S = 2048
 
 pos = Position(wd, filename, SAMPLERATE, WIN_S, HOP_S, verbose = False)
 
-s = lb.core.load(filename_wav, sr = SAMPLERATE, offset = 35, duration = 100)
-for i in range(50):
-    samples = s[0][i*CHUNK:(i+1)*CHUNK]
-    pos.find_position(samples)
+mode = 'online'
 
-
-
-# # os.system("start " + filename_wav)
-# p = pyaudio.PyAudio()
-# 
-# stream = p.open(format = pyaudio.paFloat32,
-#                 channels = 1,
-#                 rate = SAMPLERATE,
-#                 input = True,
-#                 frames_per_buffer = CHUNK, 
-#                 stream_callback = callback)
-# 
-# print("* recording")
-# 
-# stream.start_stream()
-# 
-# while stream.is_active():
-#     time.sleep(0.1)
-# print("* done recording")
-# 
-# stream.stop_stream()
-# stream.close()
-# p.terminate()
+if mode == 'offline':
+    spec_data = utils.get_wav_spectrogram(filename_wav, samplerate = SAMPLERATE, win_s = WIN_S, hop_s = HOP_S, start_sec = 217.0, end_sec = 225.0)
+    chromagram = utils.spectrogram_to_chromagram(spec_data["spectrogram"], spec_data["frequencies"])
+    fig = utils.plot_chromagram(chromagram, SAMPLERATE, HOP_S)
+    a=0
+#     s = lb.core.load(filename_wav, sr = SAMPLERATE, offset = 35, duration = 20)
+#     for i in range(50):
+#         samples = s[0][i*CHUNK:(i+1)*CHUNK]
+#         pos.find_position(samples)
+elif mode == 'online':
+# os.system("start " + filename_wav)
+    p = pyaudio.PyAudio()
+     
+    stream = p.open(format = pyaudio.paFloat32,
+                    channels = 1,
+                    rate = SAMPLERATE,
+                    input = True,
+                    frames_per_buffer = CHUNK, 
+                    stream_callback = callback)
+     
+    print("* recording")
+     
+    stream.start_stream()
+     
+    while stream.is_active():
+        time.sleep(0.1)
+    print("* done recording")
+     
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
