5b73eb328e1fa0cd42ce59c77d97a43438511df9
9552nZ <alexis.nortier@edhec.com>
2017-06-06 23:07:24 +0100

    Add files via upload

--- test_real_time_DTW.py
+++ test_real_time_DTW.py
@@ -3,7 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import utils_audio_transcript as utils
 
+class CellList():
+    '''
+    Class to handle list of cells.
+    A cell is simply a tuple of coordinates (i,j) modeling the position in
+    the cum_distance matrix.
+    '''
+    def __init__(self, rows, cols):
+        
+        self.len = len(rows)
+        assert self.len == len(cols) 
+        self.rows = rows
+        self.cols = cols    
+        
+    def __repr__(self):
+        return(np.array([np.array(self.rows).T, np.array(self.cols).T]).__repr__())   
+
+    def append(self, cell):
+        self.len += cell.len         
+        self.rows.extend(cell.rows)
+        self.cols.extend(cell.cols)
+
+    def get_cell(self, idx):        
+        return(CellList([self.rows[idx]], [self.cols[idx]]))
+    
+    def get_cell_as_tuple(self, idx):
+        return((self.rows[idx], self.cols[idx]))
+
+
 class Matcher():
+    '''
+    Implementation of the online DTW matcher as per S. Dixon.
+    Resources:
+    http://eecs.qmul.ac.uk/~simond/pub/2005/dafx05.pdf
+    https://www.eecs.qmul.ac.uk/~simond/pub/2010/Macrae-Dixon-ISMIR-2010-WindowedTimeWarping.pdf
+    http://www.cp.jku.at/research/papers/Arzt_Masterarbeit_2007.pdf
+    https://code.soundsoftware.ac.uk/projects/match/repository/entry/at/ofai/music/match/Finder.java
+    '''
     
     def __init__(self, chromagram_act):
         
@@ -27,7 +63,10 @@ class Matcher():
         self.chromagram_est.fill(np.nan)
         self.cum_distance = np.zeros((chromagram_act.shape[0], chromagram_act.shape[0]*3))
         self.cum_distance.fill(np.nan)
-         
+        
+        # Initialise the best_paths, in order to keep the best path at each iteration
+        # (one iteration correspond to one update of the live feed)
+        self.best_paths = []
         
     def select_advance_direction(self):        
         
@@ -59,10 +98,10 @@ class Matcher():
         '''
         
         if i-1 < 0:
-            return(([i],[j-1]))            
+            return(CellList([i],[j-1]))            
             
         if j-1 < 0:
-            return(([i-1],[j]))
+            return(CellList([i-1],[j]))
         
         isnan_s = np.isnan(self.cum_distance[i-1, j])
         isnan_w = np.isnan(self.cum_distance[i, j-1])
@@ -71,40 +110,48 @@ class Matcher():
         # Standard case: compute everything
         # The case with all 3 nans should not arise
         if not isnan_s and not isnan_w and not isnan_sw:
-            return(([i-1, i, i-1],[j, j-1, j-1]))
+            return(CellList([i-1, i, i-1],[j, j-1, j-1]))
         
         elif isnan_s and isnan_sw:
-            return(([i],[j-1]))
+            return(CellList([i],[j-1]))
         
         elif isnan_w and isnan_sw:
-            return(([i-1],[j]))
+            return(CellList([i-1],[j]))
         
         elif isnan_s:
-            return(([i,i-1], [j-1, j-1]))
+            return(CellList([i,i-1], [j-1, j-1]))
         
         elif isnan_w:
-            return(([i-1, i-1],[j, j-1]))
-        
-    def find_best_path_distance(self, cells):
+            return(CellList([i-1, i-1],[j, j-1]))
+
+                                                                
+    def find_best_path(self, cells, return_distance):
         '''
         The function computes the local DTW path, adjusting for the weigths.
         It relies on the cells being passed such that the diagonal is the 
         last item (if two or three elements).
+        
+        It can either return the min distance or the min path.
         '''
-        len_cells = len(cells[0])        
                     
-        if len_cells == 3:
-            return(min(self.cum_distance[cells[0][0], cells[1][0]], 
-                       self.cum_distance[cells[0][1], cells[1][1]], 
-                       2.0 * self.cum_distance[cells[0][2], cells[1][2]]))
+        if cells.len == 3:
+            d = np.array([self.cum_distance[cells.get_cell_as_tuple(0)], 
+                          self.cum_distance[cells.get_cell_as_tuple(1)], 
+                          2.0 * self.cum_distance[cells.get_cell_as_tuple(2)]])            
+                    
+        elif cells.len == 2:            
+            d = np.array([self.cum_distance[cells.get_cell_as_tuple(0)], 2.0 * self.cum_distance[cells.get_cell_as_tuple(1)]])
+            
+        else:
+            d = np.array([self.cum_distance[cells.get_cell_as_tuple(0)]])
         
-        if len_cells == 2:
-            return(min(self.cum_distance[cells[0][0], cells[1][0]], 2.0 * self.cum_distance[cells[0][1], cells[1][1]]))
+        argmin = np.argmin(d)
         
-        if len_cells == 1:
-            return(self.cum_distance[cells[0][0], cells[1][0]])
-        
-         
+        if return_distance:                
+            return(d[argmin])
+        else:
+            return(cells.get_cell(argmin))
+                            
     def update_cum_distance(self, direction):
         '''
         Taking the existing cum_distance matrix and the search direction, this function
@@ -136,7 +183,7 @@ class Matcher():
             for k in np.arange(max(idx_est-self.search_width-1, 0), idx_est+1):                
                 distance = utils.distance_chroma(chromagram_act[idx_act, :], self.chromagram_est[k, :], 1, 12)                
                 cells = self.find_cells(idx_act, k)
-                self.cum_distance[idx_act, k] = distance + self.find_best_path_distance(cells)                                                                 
+                self.cum_distance[idx_act, k] = distance + self.find_best_path(cells, True)                                                                 
             return
             
         if direction == 2:
@@ -149,7 +196,7 @@ class Matcher():
             for k in np.arange(max(idx_act-self.search_width-1, 0), idx_act+1):
                 distance = utils.distance_chroma(chromagram_act[k, :], self.chromagram_est[idx_est, :], 1, 12)
                 cells = self.find_cells(k, idx_est)
-                self.cum_distance[k, idx_est] = distance + self.find_best_path_distance(cells)
+                self.cum_distance[k, idx_est] = distance + self.find_best_path(cells, True)
             return
             
     def update_best_path(self):
@@ -162,41 +209,48 @@ class Matcher():
         idx_est = self.idx_est
         idx_act = self.idx_act
         
+        # Do not run if we are at the starting point
+        if idx_act == 0 and idx_est == 0:
+            return
+        
         # Check if the minimum distance is in the last row or in the last column
         arg_min_row = np.nanargmin(self.cum_distance[idx_act,0:idx_est+1])
         arg_min_col = np.nanargmin(self.cum_distance[0:idx_act+1,idx_est])
         
-        path_rows = []
-        path_cols = []
-        
         # Collect the final point of the DTW path
         if self.cum_distance[idx_act,arg_min_row] <= self.cum_distance[arg_min_col,idx_est]:
-            path_rows.append(idx_act)
-            path_cols.append(arg_min_row)
+            best_path = CellList([idx_act], [arg_min_row])
             idx_est = arg_min_row
         else:
-            path_rows.append(arg_min_col)
-            path_cols.append(idx_est)
+            best_path = CellList([arg_min_col], [idx_est])
             idx_act = arg_min_col
             
         # Iterate until the starting point
         while idx_act > 0 or idx_est > 0:
-            cells = self.find_cells(idx_act, idx_est)
-        
+            cells = self.find_cells(idx_act, idx_est)  
+            best_path_local = self.find_best_path(cells, False)
+            (idx_act, idx_est) = best_path_local.get_cell_as_tuple(0) 
+            best_path.append(best_path_local)
             
+        # Keep the best path for successive iterations
+        # (the history of the best paths could be dropped at a later stage) 
+        self.best_paths.append(best_path)
                                                         
     def main_matcher(self, chromagram_est_row): 
         '''
         Called for every new frame of the live feed, for which we run the online DTW
         '''
-        # Disable the matching procedure if we are at the end of the act data
-        if self.idx_act >= self.chromagram_act.shape[0] - 1:
-            return
-        
+
         self.chromagram_est[self.idx_est+1, :] = chromagram_est_row
         self.input_advanced = False
 
         while not self.input_advanced:
+            
+            # Disable the matching procedure if we are at the end of the act data
+            if self.idx_act >= self.chromagram_act.shape[0] - 1:
+                return
+            
+            # Otherwise, run the main loop
             direction = self.select_advance_direction()
             self.update_cum_distance(direction)                
         
@@ -243,8 +297,8 @@ data = matcher.cum_distance[0:500, 0:500]
 data = matcher.cum_distance[0:1000, 0:1000]
 data = matcher.cum_distance[1000:2000, 1000:2000]
 plt.pcolor(data, cmap=plt.cm.Blues, alpha=0.8, vmin=np.nanmin(data), vmax=np.nanmax(data))
-    
 
-
-
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax1.plot(matcher.best_paths[2000].rows, matcher.best_paths[2000].cols)
 
