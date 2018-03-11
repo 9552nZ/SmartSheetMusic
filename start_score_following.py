0c85a0501224810204c13a6fb00ef454f384b7a8
9552nZ <alexis.nortier@edhec.com>
2018-03-11 19:06:14 +0000

    Add files via upload

--- start_score_following.py
+++ start_score_following.py
@@ -8,19 +8,19 @@ It is called by the graphical client, and takes as input the path of the '.mid' 
     - run the online matching
     - publish the results via the socket
 '''
-
+from time import sleep
 import pyaudio
 import numpy as np
 import sys
 import zmq
-from time import sleep
 import utils_audio_transcript as utils
 from score_following import Matcher
 from music_detection import MusicDetecter
 from pandas import DataFrame
 import os # REMOVE
 import datetime # REMOVE
-import multiprocessing # REMOVE
+from multiprocessing import Process, Manager, Pool
+from _overlapped import NULL
 
 # Flag for the communication with the GUI
 # TODO : Replace by integers
@@ -28,9 +28,18 @@ STOP = b"stop"
 START = b"start"
 MATCH = b"match"
 PAUSE = b"pause"
+INIT = b"init"
 
-# Flag to see if we run on Python 3 or more
-PYTHON_LEGACY = sys.version_info < (3, 0) 
+# Start the ZMQ sockets
+context = zmq.Context()
+SOCKET_PUB = context.socket(zmq.PUB)
+PUB_ADDRESS = r"tcp://127.0.0.1:5555"
+SOCKET_SUB = context.socket(zmq.SUB)
+SOCKET_SUB.setsockopt_string(zmq.SUBSCRIBE, r'')                
+SOCKET_SUB.connect("tcp://127.0.0.1:5556")
+
+# The frequency at which we publish (in secs)
+PUB_FREQ = 0.1 
 
 class MatcherManager():
     ''' 
@@ -41,48 +50,49 @@ class MatcherManager():
     - the music detector
     '''
     def __init__(self, filename_mid_full, callback_fcn):
+
         self.chunk = int(16384/16) # The chunk of data processed per pyaudio batch        
         self.sr = utils.SR # Sample rate
         self.hop_length = utils.HOP_LENGTH 
         self.audio_format = utils.AUDIO_FORMAT_DEFAULT # The low-level audio format 
         self.detect_music = False # Set to true to only start once the MLP has detected music
-        self.callback_fcn = callback_fcn # The pyaudio callback function  
-        
-        # Start the publishing socket
-        self.bind_sockets()
+        self.callback_fcn = callback_fcn # The pyaudio callback function
          
         # Split the path of the midi input between the directory and file name.
         (wd, self.filename) = os.path.split(filename_mid_full)
-        self.wd = wd + "/" 
-     
-        # Initialise the matcher
-        self.matcher = Matcher(self.wd, self.filename, self.sr, self.hop_length, 
-                               compute_chromagram_fcn_kwargs={'n_fft':self.hop_length*2, 'n_chroma':12})
+        self.wd = wd + "/"      
         
-        # Initialise the audio data buffer
-        self.audio_data = np.array([])
+        # We initialise with a INIT status for the GUI and a PAUSE for the matcher         
+        self.status = INIT
+        self.status_matcher = PAUSE   
+        self.music_detection_diagnostic = ''                      
         
-        self.audio_data2 = np.array([]) # REMOVE
-     
-        # Initialise the music detecter
-        self.music_detection_diagnostic = ''
-        if self.detect_music:
-            self.music_detecter = MusicDetecter(utils.WD_AUDIOSET + "VerifiedDataset\\VerifiedDatasetRecorded\\", self.sr)    
-        
-        # Start the pyaudio stream 
-        self.start_stream()
-        
-        # We initialise with a STOP status for the GUI and a PAUSE for the matcher         
-        self.status = STOP
-        self.status_matcher = PAUSE                         
-        
-    def __getstate__(self):
+    def __getstate__(self):            
         '''
         Called by pickle.dump to know what attributes need to be pickled. 
         Pickle everything but the sockets and pyaudio.
         '''
         return {k: v for k, v in self.__dict__.items() if k not in ['socket_pub', 'socket_sub', 'pyaudio', 'stream', 'callback_fcn']}
     
+    def warm_up(self, status_init):
+        '''
+        Do all the expensive part of the initialisation.
+        status_init is a shared Value() between two processes.
+        '''
+        
+        # Initialise the matcher
+        self.matcher = Matcher(self.wd, self.filename, self.sr, self.hop_length, 
+                               compute_chromagram_fcn_kwargs={'n_fft':self.hop_length*2, 'n_chroma':12})
+        
+        # Initialise the audio data buffer
+        self.audio_data = np.array([])
+     
+        # Initialise the music detecter        
+        if self.detect_music:
+            self.music_detecter = MusicDetecter(utils.WD_AUDIOSET + "VerifiedDataset\\VerifiedDatasetRecorded\\", self.sr)    
+            
+        # Set the status to STOP
+        status_init['value'] = STOP
         
     def reinit(self):
         '''
@@ -91,7 +101,7 @@ class MatcherManager():
         '''         
         midi_obj = self.matcher.midi_obj
         
-        # Re-initilaise the matcher, making sure that we have the same args as the 
+        # Reinitialise the matcher, making sure that we have the same args as the 
         # ones used for initialisation.
         self.matcher = Matcher(self.wd, self.filename, self.sr, self.hop_length,
                                compute_chromagram_fcn=self.matcher.compute_chromagram_fcn,
@@ -100,24 +110,6 @@ class MatcherManager():
         
         self.matcher.midi_obj = midi_obj 
         self.status = STOP
- 
-    def bind_sockets(self):
-        '''
-        Start the ZMQ sockets
-        '''
-        context = zmq.Context()
-        self.socket_pub = context.socket(zmq.PUB)
-#         self.socket_pub.bind("tcp://*:5555")
-        self.socket_pub.bind("tcp://127.0.0.1:5555")
-        self.socket_sub = context.socket(zmq.SUB)
-        
-        # Need to use different options for python3 vs python2
-        if PYTHON_LEGACY:
-            self.socket_sub.setsockopt(zmq.SUBSCRIBE, '')
-        else:
-            self.socket_sub.setsockopt_string(zmq.SUBSCRIBE, r'')            
-        
-        self.socket_sub.connect("tcp://127.0.0.1:5556")
         
     def start_stream(self):
         '''
@@ -167,16 +159,12 @@ class MatcherManager():
         ''' 
         # Start by converting the input string to numpy array
         new_audio_data = np.fromstring(new_audio_data, dtype=utils.AUDIO_FORMAT_MAP[self.audio_format][0])
-#         new_audio_data = new_audio_data.astype('float32') #REMOVE
         
         # Keep a local buffer (we keep as many samples as the music detection procedure requires)
         # TODO : move to update_status_matcher()
         if self.detect_music:
             self.audio_data = np.append(self.audio_data, new_audio_data)        
             self.audio_data = self.audio_data[max(len(self.audio_data)-self.music_detecter.model.nb_sample, 0):len(self.audio_data)]
-            
-        # REMOVE!!!
-        self.audio_data2 = np.append(self.audio_data2, new_audio_data)
         
         # Check the status
         self.update_status_matcher()
@@ -185,22 +173,7 @@ class MatcherManager():
         
         # Run the online matching with the new data only.
         # This will be a problem if we use a larger music detection buffer, careful!!!
-        self.matcher.match_online(new_audio_data)
-            
-    def publish_position(self):
-        '''
-        Send the current position to the GUI via the socket.
-        '''
-        root_str = "Sending current position: {}         Status: {}({})"
-        print(root_str.format(self.matcher.position_tick, self.status_matcher, self.music_detection_diagnostic))
-        
-        # Need to be careful when sending the bytes representation of an int
-        # The bytes functions behaves differently in python 2 vs python 3.
-        if PYTHON_LEGACY:
-            self.socket_pub.send(bytes(self.matcher.position_tick))
-        else:                               
-            self.socket_pub.send_string(str(self.matcher.position_tick))
-            print(str(self.matcher.position_tick))
+        self.matcher.match_online(new_audio_data)                    
                 
     def save(self):
         '''
@@ -228,7 +201,7 @@ class MatcherManager():
         '''
         # Receive the instruction from the front-end
         try:
-            flag = self.socket_sub.recv(flags=zmq.NOBLOCK) # What happens when we have multiple messages?? Maybe set the SUB to CONFLATE                                           
+            flag = SOCKET_SUB.recv(flags=zmq.NOBLOCK) # What happens when we have multiple messages?? Maybe set the SUB to CONFLATE                                           
         except zmq.Again:
             flag = self.status
             
@@ -236,7 +209,8 @@ class MatcherManager():
         reached_end_act = len(self.matcher.positions) > 0 and self.matcher.positions[-1] >= self.matcher.nb_obs_feature_act
         flag = STOP if reached_end_act else flag  
                         
-        if self.status == STOP and flag == STOP:
+        # If we have not yet received any instruction, or if the matcher is not initialised do nothing                        
+        if (self.status == STOP and flag == STOP) or (self.status == INIT):
             pass
         
         elif (self.status == START and flag == START) or (self.status == STOP and flag == START): 
@@ -245,14 +219,37 @@ class MatcherManager():
             
         elif self.status == START and flag == STOP:
             # Send a stop instruction, save and reinitialise
-            self.socket_pub.send(bytes(STOP))
+            publish(bytes(STOP)) # BYTES OR STR?????
             self.save()            
             self.reinit()            
             self.music_detection_diagnostic = ''            
             
         else:
-            raise(ValueError('Unexpected status ({}) or flag ({})'.format(self.status, flag)))                    
-                    
+            raise(ValueError('Unexpected status ({}) or flag ({})'.format(self.status, flag)))
+        
+def check_warm_up_status(status):
+    '''
+    While the manager is not initialised, publish the INIT flag.
+    The status variable is shared between the two processes.
+    '''        
+    SOCKET_PUB.bind(PUB_ADDRESS)
+    while status['value'] == INIT:        
+        publish(INIT)
+        sleep(PUB_FREQ)
+            
+        SOCKET_PUB.unbind(PUB_ADDRESS)
+            
+def publish(data):
+    '''
+    Send instructions to the front end via the socket.
+    '''        
+    root_str = "Sending: {}"
+    print(root_str.format(str(data)))   # BYTES OR STR?????
+    
+    # Need to be careful when sending the bytes representation of an int
+    # The bytes functions behaves differently in python 2 vs python 3.            
+    SOCKET_PUB.send_string(str(data))
+                                
             
 def callback(in_data, frame_count, time_info, flag):
     '''
@@ -263,7 +260,7 @@ def callback(in_data, frame_count, time_info, flag):
     global matcher_manager
     matcher_manager.callback_fcn(matcher_manager, in_data)
     return (None, pyaudio.paContinue)
-            
+
 if __name__ == '__main__':    
     # Input the target '.mid' file of the score that we want to follow 
     if len(sys.argv) > 1:
@@ -272,19 +269,39 @@ if __name__ == '__main__':
     else:
         filename_mid = utils.WD + '\Samples\IMSLP//' +  'Beethoven_Moonlight_Sonata_Op._27_No._2_Mvt._2.wav'
         callback_fcn = MatcherManager.callback_main
-     
-    matcher_manager = MatcherManager(filename_mid, callback_fcn)    
+ 
+    # Initialise the MatcherManager (fast) 
+    matcher_manager = MatcherManager(filename_mid, callback_fcn)
     
-    t1 = datetime.datetime.now()
+    context2 = zmq.Context()
+    socket = context2.socket(zmq.REQ)
+    socket.connect("tcp://127.0.0.1:5557")
+    socket.send(b"Hello")
+    message = socket.recv()        
+    
+    # We need to update the front-end about the warming up (slow)
+    # Start an ancillary process to monitor the progress of the 
+    # warming up.
+    status = Manager().dict()
+    status['value'] = INIT                                     
+    process_check_warm_up = Process(target=check_warm_up_status, args=(status, ))
+    process_check_warm_up.start()    
+    
+    matcher_manager.warm_up(status)
+    
+    # Once the warm-up is done, we can bind the socket in the main thread
+    while process_check_warm_up.is_alive():
+        sleep(PUB_FREQ)
+    
+    SOCKET_PUB.bind(PUB_ADDRESS)    
+
+    # Start the pyaudio stream 
+    matcher_manager.start_stream()    
+     
     while matcher_manager.stream.is_active():
-        matcher_manager.publish_position()
-        sleep(matcher_manager.chunk/float(matcher_manager.sr))
-        t2 = datetime.datetime.now()
-        
-#         if (t2-t1).total_seconds() > 10.0:            
-#             break
-                
+        publish(matcher_manager.matcher.position_tick)
+        sleep(PUB_FREQ)
+                 
     matcher_manager.stream.stop_stream()
     matcher_manager.stream.close()    
-    #np.save(r"C:\Users\Alexis\Business\SmartSheetMusic//live.npy", matcher_manager.audio_data2)
-    a=1    
+    SOCKET_PUB.unbind(PUB_ADDRESS)
